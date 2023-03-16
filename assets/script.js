40c2642c2974660189ba7ee0534ee99c6cca2bee
Hank Richter <hankrichter97@gmail.com>
2023-03-16 15:57:46 -0400

    Added variables and the fetch function for the single day weather report

---
+++ assets/script.js
@@ -0,0 +1,22 @@
+const cityInput = document.querySelector(".cityInput")
+const searchButton = document.querySelector(".searchButton")
+const weatherBaseURL = "https://api.openweathermap.org/data/2.5/weather?q="
+const weatherAPIKey = "&appid=3ea36f8f67b0fbc770aed01a9cb37854"
+const imperialUnits = "&units=imperial"
+
+function weatherFetcher(){
+
+    const dailyWeather = weatherBaseURL + cityInput.value + imperialUnits + weatherAPIKey
+
+    fetch(dailyWeather, {
+    cache: 'reload',
+    })
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+    });
+}
+
+searchButton.addEventListener("click", weatherFetcher);
