dc8679a6e401c815eb1a9569046e8d0f0f4c1627
Hank Richter <120127039+HankRichter@users.noreply.github.com>
2023-03-17 07:00:34 -0400

    added code to the function to get text content to appear once a city is selected

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,31 @@
-const cityInput = document.querySelector(".cityInput")
-const searchButton = document.querySelector(".searchButton")
-const dailyWeatherContainer = document.querySelector(".container3")
-const weatherBaseURL = "https://api.openweathermap.org/data/2.5/weather?q="
-const weatherAPIKey = "&appid=3ea36f8f67b0fbc770aed01a9cb37854"
-const imperialUnits = "&units=imperial"
+const cityInput = document.querySelector(".cityInput");
+const searchButton = document.querySelector(".searchButton");
+const dailyWeatherContainer = document.querySelector(".container3");
+const currentCity = document.querySelector(".current-city");
+const currentTemp = document.querySelector(".current-temp");
+const currentWind = document.querySelector(".current-wind");
+const currentHumidity = document.querySelector(".current-humidity");
+const weatherBaseURL = "https://api.openweathermap.org/data/2.5/weather?q=";
+const weatherAPIKey = "&appid=3ea36f8f67b0fbc770aed01a9cb37854";
+const imperialUnits = "&units=imperial";
 
-function weatherFetcher(){
+function weatherFetcher() {
+  const dailyWeather =
+    weatherBaseURL + cityInput.value + imperialUnits + weatherAPIKey;
 
-    const dailyWeather = weatherBaseURL + cityInput.value + imperialUnits + weatherAPIKey
-
-    fetch(dailyWeather, {
-    cache: 'reload',
-    })
+  fetch(dailyWeather, {
+    cache: "reload",
+  })
     .then(function (response) {
-        return response.json();
+      return response.json();
     })
     .then(function (data) {
-        console.log(data);
+      console.log(data);
+      const icon = document.createElement("img");
+      currentCity.textContent = data.name + "" + data.weather[0].icon;
+      currentTemp.textContent = "Temp:" + data.main.temp;
+      currentWind.textContent = "Wind:" + data.wind.speed + " MPH";
+      currentHumidity.textContent = "Humidity:" + data.main.humidity + " %";
     });
 }
 
